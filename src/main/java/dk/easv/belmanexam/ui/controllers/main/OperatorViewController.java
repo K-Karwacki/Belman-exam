b7f902bec402ca7b9d47d2ca8647834f0d1739fc
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-05-05 15:38:13 +0200

    set root in operator view

--- src/main/java/dk/easv/belmanexam/ui/controllers/main/OperatorViewController.java
+++ src/main/java/dk/easv/belmanexam/ui/controllers/main/OperatorViewController.java
@@ -1,4 +1,17 @@
 package dk.easv.belmanexam.ui.controllers.main;
 
+import dk.easv.belmanexam.ui.FXMLPath;
+import dk.easv.belmanexam.ui.ViewManager;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class OperatorViewController {
+    @FXML
+    private BorderPane root;
+
+
+    public void initialize(){
+        ViewManager.INSTANCE.setStageRoot(root);
+        ViewManager.INSTANCE.switchDashboard(FXMLPath.ENTER_ORDER_DASHBOARD, "BelSign");
+    }
 }
