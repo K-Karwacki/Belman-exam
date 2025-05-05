b0fa1015042f514903667fde422b87e931ccb838
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-05-05 10:21:20 +0200

    Exception

--- src/main/java/dk/easv/belmanexam/exceptions/PhotoException.java
+++ src/main/java/dk/easv/belmanexam/exceptions/PhotoException.java
@@ -1,7 +1,7 @@
 package dk.easv.belmanexam.exceptions;
 
 public class PhotoException extends Exception {
-    public PhotoException(Exception message) {
-        super(message);
+    public PhotoException(Exception e) {
+        super(e.getMessage());
     }
 }
