a11ed0aee01ac3a062de0635c1f7d973c2877d60
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-05-06 10:49:58 +0200

    Changed enum

---
+++ src/main/java/dk/easv/belmanexam/utils/PasswordHasher.java
@@ -0,0 +1,13 @@
+package dk.easv.belmanexam.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordHasher
+{
+  public static String hashPassword(String password) {
+    if (password == null) {
+      throw new IllegalArgumentException("Password cannot be null");
+    }
+    return BCrypt.hashpw(password, BCrypt.gensalt(12));
+  }
+}
