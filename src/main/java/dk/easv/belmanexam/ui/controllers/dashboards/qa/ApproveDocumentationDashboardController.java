b05ece8a7cd96e001c9be7ee2bb464b31d45f68b
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-05-07 11:33:17 +0200

    QA changes

--- src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/ApproveDocumentationDashboardController.java
+++ src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/ApproveDocumentationDashboardController.java
@@ -1,4 +1,9 @@
 package dk.easv.belmanexam.ui.controllers.dashboards.qa;
 
+import javafx.fxml.FXML;
+
 public class ApproveDocumentationDashboardController {
+    @FXML
+    private void showParentView() {
+    }
 }
--- src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/DocumentationDashboardController.java
+++ src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/DocumentationDashboardController.java
@@ -1,4 +1,13 @@
 package dk.easv.belmanexam.ui.controllers.dashboards.qa;
 
+import dk.easv.belmanexam.ui.controllers.components.OrderListComponent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
 public class DocumentationDashboardController {
+    @FXML
+    private TableView<OrderListComponent> tableViewDocumentationContainer;
+
+
 }
