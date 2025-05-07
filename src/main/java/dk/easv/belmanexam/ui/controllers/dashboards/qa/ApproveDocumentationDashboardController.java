de61d07f79db2a62573d61e089e740f99ba22a80
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-05-07 12:04:01 +0200

    Changes to QA documentation dashboard

--- src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/ApproveDocumentationDashboardController.java
+++ src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/ApproveDocumentationDashboardController.java
@@ -1,9 +1,20 @@
 package dk.easv.belmanexam.ui.controllers.dashboards.qa;
 
+import dk.easv.belmanexam.ui.FXMLManager;
+import dk.easv.belmanexam.ui.FXMLPath;
+import dk.easv.belmanexam.ui.controllers.components.OrderListComponent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.util.Pair;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ApproveDocumentationDashboardController {
     @FXML
     private void showParentView() {
     }
+
+
 }
--- src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/DocumentationDashboardController.java
+++ src/main/java/dk/easv/belmanexam/ui/controllers/dashboards/qa/DocumentationDashboardController.java
@@ -1,13 +1,37 @@
 package dk.easv.belmanexam.ui.controllers.dashboards.qa;
 
+import dk.easv.belmanexam.ui.FXMLManager;
+import dk.easv.belmanexam.ui.FXMLPath;
 import dk.easv.belmanexam.ui.controllers.components.OrderListComponent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.util.Pair;
 
-public class DocumentationDashboardController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DocumentationDashboardController implements Initializable {
     @FXML
-    private TableView<OrderListComponent> tableViewDocumentationContainer;
+    private FlowPane flowPaneOrderList;
 
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // @ToDo -> Remove mock data later
+        Pair<Parent, OrderListComponent> p1 = FXMLManager.INSTANCE.loadFXML(FXMLPath.ORDER_LIST_COMPONENT);
+        p1.getValue().setOrderNumber("2024/12/1113");
+//        p1.getValue().set
+        Pair<Parent, OrderListComponent> p2 = FXMLManager.INSTANCE.loadFXML(FXMLPath.ORDER_LIST_COMPONENT);
+        p2.getValue().setOrderNumber("2025/09/1941");
+        Pair<Parent, OrderListComponent> p3 = FXMLManager.INSTANCE.loadFXML(FXMLPath.ORDER_LIST_COMPONENT);
+        p3.getValue().setOrderNumber("2025/21/1231");
+        Pair<Parent, OrderListComponent> p4 = FXMLManager.INSTANCE.loadFXML(FXMLPath.ORDER_LIST_COMPONENT);
+        p4.getValue().setOrderNumber("2025/11/1331");
+        Pair<Parent, OrderListComponent> p5 = FXMLManager.INSTANCE.loadFXML(FXMLPath.ORDER_LIST_COMPONENT);
+        p5.getValue().setOrderNumber("2022/10/101");
+        flowPaneOrderList.getChildren().addAll(p1.getKey(), p2.getKey(), p3.getKey(), p4.getKey(), p5.getKey());
+    }
 }
