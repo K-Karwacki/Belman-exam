86b43690ea594e210791cee4e0a3601cc61261e8
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2025-05-14 11:10:49 +0200

    documentation preview dashboard qa and its controller

---
+++ src/main/java/dk/easv/belmanexam/ui/controllers/qa/dashboards/DocumentationPreviewController.java
@@ -0,0 +1,16 @@
+package dk.easv.belmanexam.ui.controllers.qa.dashboards;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+
+public class DocumentationPreviewController {
+    @FXML
+    private ScrollPane previewContainerPane;
+
+    public void onClickGoBack(MouseEvent mouseEvent) {
+    }
+
+    public void onClickConfirm(MouseEvent mouseEvent) {
+    }
+}
